6b8824d654d5ed7f9b95bf80b99706cbc082567c
Raúl Álvarez Pinilla <ralvarep@users.noreply.github.com>
2016-02-03 11:56:04 +0100

    Restored original thread assignment based on current load

--- opennop/opennop-daemon/opennopd/sessionmanager.c
+++ opennop/opennop-daemon/opennopd/sessionmanager.c
@@ -143,7 +143,7 @@ struct session *insertsession(__u32 largerIP, __u16 largerIPPort,
 	/*
 	 * What queue will the packets for this session go to?
 	 */
-/* COMMENTED. This was the original code, inserting new sessions considering current workload
+
 	queuenum = 0;
 
 	for (i = 0; i < get_workers(); i++) {
@@ -161,13 +161,12 @@ struct session *insertsession(__u32 largerIP, __u16 largerIPPort,
 			}
 		}
 	}
-*/
 
 // New behaviour: a hash depending on source and destination IP addresses is computed. The allocated queue depends on this hash.
-	unsigned char hashflow;
-	hashflow = (largerIP & 0xff) ^((largerIP >> 8) & 0xff) ^ ((largerIP >> 16) & 0xff) ^((largerIP >> 24) & 0xff) ; 
-	hashflow = hashflow ^(smallerIP & 0xff) ^((smallerIP >> 8) & 0xff) ^ ((smallerIP >> 16) & 0xff) ^((smallerIP >> 24) & 0xff) ; 
-	queuenum = hashflow % get_workers();
+//	unsigned char hashflow;
+//	hashflow = (largerIP & 0xff) ^((largerIP >> 8) & 0xff) ^ ((largerIP >> 16) & 0xff) ^((largerIP >> 24) & 0xff) ; 
+//	hashflow = hashflow ^(smallerIP & 0xff) ^((smallerIP >> 8) & 0xff) ^ ((smallerIP >> 16) & 0xff) ^((smallerIP >> 24) & 0xff) ; 
+//	queuenum = hashflow % get_workers();
 // End new behaviour
 
 	if (DEBUG_SESSIONMANAGER_INSERT == true) {
