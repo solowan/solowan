22c3b17719da0365f30b34e05d6d44d51c78ba96
davidfdezc <davidfdezc@github.com>
2016-03-18 03:15:32 +0100

    /etc/opennop/opennop-addflows.sh modified

--- opennop/opennop-daemon/etc/opennop/opennop-addflows.sh
+++ opennop/opennop-daemon/etc/opennop/opennop-addflows.sh
@@ -3,31 +3,31 @@
 # 
 # WEB CLIENT: process traffic from  a local web client connecting to a remote server in port 80
 #
-iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 80
-iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 80
+#iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 80
+#iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 80
 
 #
 # WEB SERVER: process traffic to a local web server running on port 80
 #
-iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 80
-iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 80
+#iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 80
+#iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 80
 
 #
 # FTP CLIENT: control traffic
 #
-iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 21
-iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 21
+#iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 21
+#iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 21
 
 #
 # FTP SERVER: control traffic
 #
-iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 21
-iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 21
+#iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 21
+#iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 21
 
 #
 # FTP: data traffic
 #
-iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 9000:9499
-iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 9000:9499
-iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 9000:9499
-iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 9000:9499
+#iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 9000:9499
+#iptables -v -A OUTPUT     -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 9000:9499
+#iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --sport 9000:9499
+#iptables -v -A PREROUTING -t mangle -j NFQUEUE --queue-num 0 --queue-bypass -p TCP --dport 9000:9499
